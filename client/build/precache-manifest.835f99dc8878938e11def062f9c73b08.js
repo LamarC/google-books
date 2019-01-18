befe8d72cf2f2c6bf8ecf2054d1ae89903bbbc49
LamarC <dviann89@gmail.com>
2019-01-18 16:38:23 -0600

    First commit

---
+++ client/build/precache-manifest.835f99dc8878938e11def062f9c73b08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7dcb8ef9476c823ccb10",
+    "url": "/static/js/main.7dcb8ef9.chunk.js"
+  },
+  {
+    "revision": "23b9899313a1a5c49e3d",
+    "url": "/static/js/1.23b98993.chunk.js"
+  },
+  {
+    "revision": "7dcb8ef9476c823ccb10",
+    "url": "/static/css/main.b4c1b154.chunk.css"
+  },
+  {
+    "revision": "89d9fa89a8063b7c7ceac386bb5dc610",
+    "url": "/index.html"
+  }
+];
