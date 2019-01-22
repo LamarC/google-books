80d2f9f68151f130f27aa331c964701ed116c980
LamarC <dviann89@gmail.com>
2019-01-22 14:25:27 -0600

    Form on src folder created

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,3 +1,22 @@
 export * from "./Input";
 export * from "./TextArea";
 export * from "./FormBtn";
+
+function Form({
+    handleInput, handleSubmit
+}) {
+    return (
+        <form>
+            <div>
+                <label>
+
+                </label>
+            </div>
+            <input>
+                id="Title"
+            </input>
+        </form>
+    );
+}
+
+export default Form;
